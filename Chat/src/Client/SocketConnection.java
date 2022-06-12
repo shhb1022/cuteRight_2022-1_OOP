b86512cc7e07a9bafc8db1904fea9d6633094350
doldam42 <doldam42@github.com>
2022-06-12 14:26:12 +0900

    demo

--- Chat/src/Client/SocketConnection.java
+++ Chat/src/Client/SocketConnection.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.*;
 
 public class SocketConnection {
-    public static final String SERVER_IP = "0000.0000.0000";
+    public static final String SERVER_IP = "192.168.0.59";
     public static boolean socketConnect = false;
     public static Socket socket = null;
 
